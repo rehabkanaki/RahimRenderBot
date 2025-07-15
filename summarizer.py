516e740751690b64a2d40787e21c5204b65e37c8
rehabkanaki <rehabkanaki@gmail.com>
2025-07-15 14:30:37 +0200

    Delete summarizer.py

--- summarizer.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-# تحميل النموذج فقط عند أول تشغيل
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-def summarize_text(text, max_len=130, min_len=30):
-    result = summarizer(text, max_length=max_len, min_length=min_len, do_sample=False)
-    return result[0]['summary_text']
